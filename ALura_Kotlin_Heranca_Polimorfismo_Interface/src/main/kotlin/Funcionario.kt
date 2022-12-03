cc196e7826fba3df79666adb3e23e4d4d7ae373f
alxmart <alxmartinez@gmail.com>
2022-12-03 19:16:59 -0300

    Tests and exercises.

--- ALura_Kotlin_Heranca_Polimorfismo_Interface/src/main/kotlin/Funcionario.kt
+++ ALura_Kotlin_Heranca_Polimorfismo_Interface/src/main/kotlin/Funcionario.kt
@@ -1,9 +1,16 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double
+    val salario: Double,
+    val tipo:  //  0 - Funcionario, 1 - Gerente , 2 - Diretor
 ) {
+
     fun bonificacao(): Double {
-        return salario * 0.1
+
+        when (tipo) {
+            0 -> return salario * 0.1
+            1 -> return salario * 0.2
+            else -> return salario * 0.3
+        }
     }
 }
